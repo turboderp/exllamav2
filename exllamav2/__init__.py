2617b6c01250b48d88a1235cd5eff529769196ad
turboderp <turboderp@users.noreply.github.com>
2023-09-10 09:02:05 +0200

    Setuptools script

--- exllamav2/__init__.py
+++ exllamav2/__init__.py
@@ -4,5 +4,3 @@ from exllamav2.model import ExLlamaV2
 from exllamav2.cache import ExLlamaV2Cache
 from exllamav2.config import ExLlamaV2Config
 from exllamav2.tokenizer import ExLlamaV2Tokenizer
-
-
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+version = "0.0.0"
+
+setup(
+    name = "exllamav2",
+    version = version,
+    packages = ["exllamav2", "exllamav2.generator"],
+    url = "https://github.com/turboderp/exllamav2",
+    license = "AGPL",
+    author = "bb",
+    install_requires = [
+        "pandas",
+        "fastparquet",
+        "torch>=2.0.1",
+        "safetensors>=0.3.2",
+        "sentencepiece>=0.1.97",
+    ],
+    include_package_data = True,
+)
