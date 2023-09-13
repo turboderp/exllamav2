c221ec3630aa519fdb9b952d15d4b61a6649b509
Jeff Kerr <gojefferson@users.noreply.github.com>
2023-09-13 11:25:49 -0400

    add comment on model.load() usage

--- examples/inference.py
+++ examples/inference.py
@@ -26,6 +26,9 @@ config.prepare()
 
 model = ExLlamaV2(config)
 print("Loading model: " + model_directory)
+
+# allocate 18 GB to CUDA:0 and 24 GB to CUDA:1.
+# (Call `model.load()` if using a single GPU.)
 model.load([18, 24])
 
 tokenizer = ExLlamaV2Tokenizer(config)
