23a05b20e32b9b65da25fe5656d058b9ece9daa1
jllllll <3887729+jllllll@users.noreply.github.com>
2023-09-14 11:51:48 -0500

    Allow pre-compiling CUDA extensions.

--- setup.py
+++ setup.py
@@ -1,4 +1,47 @@
-from setuptools import setup
+from setuptools import setup, Extension
+from torch.utils import cpp_extension
+import os
+
+extension_name = "exllamav2_ext"
+verbose = False
+ext_debug = False
+
+precompile = 'EXLLAMA_NOCOMPILE' not in os.environ
+
+windows = (os.name == "nt")
+
+extra_cflags = ["/Ox"] if windows else ["-O3"]
+
+if ext_debug:
+    extra_cflags += ["-ftime-report", "-DTORCH_USE_CUDA_DSA"]
+
+extra_compile_args = {
+    "cxx": extra_cflags,
+    "nvcc": ["-lineinfo", "-O3"],
+}
+
+setup_kwargs = {
+    "ext_modules": [
+        cpp_extension.CUDAExtension(
+            extension_name,
+            [
+                "exllamav2/exllamav2_ext/ext.cpp",
+                "exllamav2/exllamav2_ext/cuda/pack_tensor.cu",
+                "exllamav2/exllamav2_ext/cuda/quantize.cu",
+                "exllamav2/exllamav2_ext/cuda/q_matrix.cu",
+                "exllamav2/exllamav2_ext/cuda/q_attn.cu",
+                "exllamav2/exllamav2_ext/cuda/q_mlp.cu",
+                "exllamav2/exllamav2_ext/cuda/q_gemm.cu",
+                "exllamav2/exllamav2_ext/cuda/rms_norm.cu",
+                "exllamav2/exllamav2_ext/cuda/rope.cu",
+                "exllamav2/exllamav2_ext/cpp/quantize_func.cpp",
+                "exllamav2/exllamav2_ext/cpp/sampling.cpp"
+            ],
+            extra_compile_args=extra_compile_args,
+            libraries=["cublas"] if windows else [],
+        )],
+    "cmdclass": {"build_ext": cpp_extension.BuildExtension}
+} if precompile else {}
 
 version = "0.0.3"
 
@@ -18,4 +61,6 @@ setup(
         "sentencepiece>=0.1.97",
     ],
     include_package_data = True,
+    verbose = verbose,
+    **setup_kwargs,
 )
