ad7b0173e2e3f769577c5926445b7ef21dfbeda8
turboderp <turboderp@users.noreply.github.com>
2023-09-16 11:45:06 +0200

    MLP: Don't return unnecessary intermediate values during quant

--- exllamav2/mlp.py
+++ exllamav2/mlp.py
@@ -147,10 +147,10 @@ class ExLlamaV2MLP(ExLlamaV2Module):
 
         if intermediates:
             return {"post_norm": post_norm,
-                    "gate": gate,
-                    "up": up,
+                    # "gate": gate,
+                    # "up": up,
                     "pre_down": y,
-                    "down": down,
+                    # "down": down,
                     "hidden_states": hidden_states}
         else:
             return hidden_states
