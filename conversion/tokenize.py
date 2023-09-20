9ad2840441cf81c9b0f12dd5bb5f6fabeae472a9
turboderp <turboderp@users.noreply.github.com>
2023-09-20 10:03:11 +0200

    Remove old disabled code

--- conversion/tokenize.py
+++ conversion/tokenize.py
@@ -38,27 +38,12 @@ def get_tokens(num_rows, length, filename, tokenizer):
 def tokenize(job, save_fn, tokenizer, measure = False):
 
     cal_ds = job["cal_dataset"]
-    # eval_ds = job["eval_dataset"]
-    # one_file = cal_ds == eval_ds
     rows = job["measurement_rows"] if measure else job["dataset_rows"]
     length = job["measurement_length"] if measure else job["length"]
 
-    # if one_file:
-    #
-    #     all_tokens = get_tokens(rows * 2, length, cal_ds, tokenizer)
-    #     cal_tokens = all_tokens[:rows, :]
-    #     eval_tokens = all_tokens[rows:, :]
-    #
-    # else:
-    #
     cal_tokens = get_tokens(rows, length, cal_ds, tokenizer)
-    # eval_tokens = get_tokens(rows, length, eval_ds, tokenizer)
 
     cal_filename = os.path.join(job["out_dir"], "cal_data.safetensors")
-    # eval_filename = os.path.join(job["out_dir"], "eval_data.safetensors")
     cal_dict = { "input_ids": cal_tokens }
-    # eval_dict = { "input_ids": eval_tokens }
     save_file(cal_dict, cal_filename)
-    # save_file(eval_dict, eval_filename)
     job["cal_filename"] = cal_filename
-    # job["eval_filename"] = eval_filename
