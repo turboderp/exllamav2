5886047b154f4fea1a885e7c4b5e7602565959ee
turboderp <turboderp@users.noreply.github.com>
2023-11-22 07:07:48 +0100

    Don't update setuptools

--- examples/inference.py
+++ examples/inference.py
@@ -39,7 +39,8 @@ generator = ExLlamaV2BaseGenerator(model, cache, tokenizer)
 # Generate some text
 
 settings = ExLlamaV2Sampler.Settings()
-settings.temperature = 0.85
+settings.temperature = 1.85
+settings.temperature_last = True
 settings.top_k = 50
 settings.top_p = 0.8
 settings.token_repetition_penalty = 1.15
