008a0bb7774d9eef428d14f58a74c1eb0b73bd10
Colin <ckealty1182@gmail.com>
2024-02-20 19:41:57 -0500

    Fix converting files with docker command

--- util/convert_safetensors.py
+++ util/convert_safetensors.py
@@ -1,15 +1,19 @@
 import torch
-import argparse, os
+import argparse, os, glob
 from safetensors.torch import save_file
 
 parser = argparse.ArgumentParser(description="Convert .bin/.pt files to .safetensors")
-parser.add_argument("--unshare", action = "store_true", help="Detach tensors to prevent any from sharing memory")
-parser.add_argument("input_files", nargs='+', type=str, help="Input file(s)")
+parser.add_argument("--unshare", action="store_true", help="Detach tensors to prevent any from sharing memory")
+parser.add_argument("input_files", nargs="+", type=str, help="Input file(s)")
 args = parser.parse_args()
 
-for file in args.input_files:
+tensor_files = []
+for file_pattern in args.input_files:
+    tensor_files.extend(glob.glob(file_pattern))
+
+for file in tensor_files:
     print(f" -- Loading {file}...")
-    state_dict = torch.load(file, map_location = "cpu")
+    state_dict = torch.load(file, map_location="cpu")
 
     if args.unshare:
         for k in state_dict.keys():
@@ -17,4 +21,4 @@ for file in args.input_files:
 
     out_file = os.path.splitext(file)[0] + ".safetensors"
     print(f" -- Saving {out_file}...")
-    save_file(state_dict, out_file, metadata = {'format': 'pt'})
+    save_file(state_dict, out_file, metadata={"format": "pt"})
