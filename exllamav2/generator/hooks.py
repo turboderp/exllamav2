cb37a1f90e3ce33fc3c8d30b9e585a6a40712f2e
turboderp <11859846+turboderp@users.noreply.github.com>
2024-03-24 10:04:17 +0100

    Post sampling hook, type hinting

--- exllamav2/generator/hooks.py
+++ exllamav2/generator/hooks.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import torch
 from typing import Callable
 from dataclasses import dataclass, field
@@ -5,12 +6,12 @@ from dataclasses import dataclass, field
 @dataclass
 class ExLlamaV2PostSamplingResult:
 
-    sampled_token: torch.Tensor = None
-    sampled_prob: torch.Tensor = None
-    candidate_tokens: torch.Tensor = None
-    candidate_probs: torch.Tensor = None
-    logits: torch.Tensor = None
+    sampled_token: torch.Tensor | None = None
+    sampled_prob: torch.Tensor | None = None
+    candidate_tokens: torch.Tensor | None = None
+    candidate_probs: torch.Tensor | None = None
+    logits: torch.Tensor | None = None
 
-    feed_filters = True
+    feed_filters: bool = True
 
 ExLlamaV2PostSamplingHook = Callable[[ExLlamaV2PostSamplingResult], None]
