029259030ca7e4cdfd4d3f7b4697510214d447f6
turboderp <11859846+turboderp@users.noreply.github.com>
2024-03-29 18:16:09 +0100

    Support DBRX

--- exllamav2/architecture.py
+++ exllamav2/architecture.py
@@ -10,6 +10,10 @@ layer_keys_llama_attn = [["self_attn.q_proj"],
                          ["self_attn.k_proj"],
                          ["self_attn.v_proj"],
                          ["self_attn.o_proj"]]
+layer_keys_dbrx_attn = [["self_attn.Wqkv", "self_attn.q_proj"],
+                        ["self_attn.Wqkv", "self_attn.k_proj"],
+                        ["self_attn.Wqkv", "self_attn.v_proj"],
+                        ["self_attn.o_proj"]]
 layer_keys_llama_mlp = [["mlp.down_proj"],
                         ["mlp.gate_proj"],
                         ["mlp.up_proj"]]
@@ -17,6 +21,10 @@ layer_keys_mixtral_mlp = [["block_sparse_moe.experts.*.w1"],
                           ["block_sparse_moe.experts.*.w2"],
                           ["block_sparse_moe.experts.*.w3"],
                           ["block_sparse_moe.gate"]]
+layer_keys_dbrx_mlp = [["block_sparse_moe.experts.*.v1", "block_sparse_moe.experts.v1"],
+                       ["block_sparse_moe.experts.*.w1", "block_sparse_moe.experts.w1"],
+                       ["block_sparse_moe.experts.*.w2", "block_sparse_moe.experts.w2"],
+                       ["block_sparse_moe.gate"]]
 layer_keys_llama_mlp_swiglu = [["mlp.swiglu.w12"],
                                ["mlp.swiglu.w3"]]
 layer_keys_starcoder2_mlp = [["mlp.c_fc"],
@@ -28,6 +36,18 @@ expect_keys_gemma = [["model.norm"],
                      ["model.embed_tokens"]]
 expect_keys_starcoder2 = [["model.norm"],
                           ["model.embed_tokens"]]
+
+dbrx_keymap = [("transformer.", "model."),
+               (".blocks.", ".layers."),
+               (".ffn.experts.mlp.", ".block_sparse_moe.experts."),
+               (".ffn.router.layer.", ".block_sparse_moe.gate."),
+               (".norm_attn_norm.norm_1.", ".input_layernorm."),
+               (".norm_attn_norm.norm_2.", ".post_attention_layernorm."),
+               (".norm_attn_norm.attn.", ".self_attn."),
+               (".out_proj.", ".o_proj."),
+               (".norm_f.", ".norm."),
+               (".wte.", ".embed_tokens.")]
+
 
 class ExLlamaV2ArchParams:
 
@@ -331,6 +351,40 @@ class ExLlamaV2ArchParams:
             self.rope_neox_style = False
             self.keymap = None
             self.fused_qkv_key = None
+
+        # DBRX
+
+        if arch_string == "DbrxForCausalLM":
+            arch_recognized = True
+            self.keymap = dbrx_keymap
+            self.layer_keys += \
+                layer_keys_llama_norms + \
+                layer_keys_dbrx_attn + \
+                layer_keys_dbrx_mlp
+            self.expect_keys += \
+                expect_keys_llama
+            self.norm_eps_key = None
+            self.attention_bias_qkv = False
+            self.attention_bias_o = False
+            self.mlp_bias = False
+            self.mlp_gate = True
+            self.mlp_key_gate = ".block_sparse_moe.experts.*.w1"
+            self.mlp_key_up = ".block_sparse_moe.experts.*.v1"
+            self.mlp_key_down = ".block_sparse_moe.experts.*.w2"
+            self.mlp_key_expert_gate = ".block_sparse_moe.gate"
+            self.mlp_act_func = "silu"
+            self.is_moe = True
+            self.norm = "layernorm"
+            self.lm_head_key = "lm_head"
+            self.normalize_embeddings = False
+            self.norm_key_1 = ".input_layernorm"
+            self.norm_key_2 = ".post_attention_layernorm"
+            self.norm_constant_bias = 0
+            self.parallel_decoder_blocks = False
+            self.requires_bos = False
+            self.rope_neox_style = True
+            self.keymap = dbrx_keymap
+            self.fused_qkv_key = "Wqkv"
 
         # Llama (default + fallback)
 
