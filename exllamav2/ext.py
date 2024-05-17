fb78d98cfca90ca682f0707e10aa15f43857dd52
turboderp <11859846+turboderp@users.noreply.github.com>
2024-05-17 09:04:23 +0200

    Suppress TORCH_CUDA_ARCH_LIST warning in Torch 2.3.0+

--- exllamav2/ext.py
+++ exllamav2/ext.py
@@ -8,6 +8,35 @@ extension_name = "exllamav2_ext"
 verbose = False  # Print wall of text when compiling
 ext_debug = False  # Compile with debug options
 
+# Since Torch 2.3.0 an annoying warning is printed every time the C++ extension is loaded, unless the
+# TORCH_CUDA_ARCH_LIST variable is set. The default behavior from pytorch/torch/utils/cpp_extension.py
+# is copied in the function below, but without the warning.
+
+def maybe_set_arch_list_env():
+
+    if os.environ.get('TORCH_CUDA_ARCH_LIST', None):
+        return
+
+    arch_list = []
+    for i in range(torch.cuda.device_count()):
+        capability = torch.cuda.get_device_capability(i)
+        supported_sm = [int(arch.split('_')[1])
+                        for arch in torch.cuda.get_arch_list() if 'sm_' in arch]
+        max_supported_sm = max((sm // 10, sm % 10) for sm in supported_sm)
+        # Capability of the device may be higher than what's supported by the user's
+        # NVCC, causing compilation error. User's NVCC is expected to match the one
+        # used to build pytorch, so we use the maximum supported capability of pytorch
+        # to clamp the capability.
+        capability = min(max_supported_sm, capability)
+        arch = f'{capability[0]}.{capability[1]}'
+        if arch not in arch_list:
+            arch_list.append(arch)
+    arch_list = sorted(arch_list)
+    arch_list[-1] += '+PTX'
+
+    os.environ["TORCH_CUDA_ARCH_LIST"] = ";".join(arch_list)
+
+
 # Determine if we're on Windows
 
 windows = (os.name == "nt")
@@ -155,6 +184,8 @@ if build_jit:
     sources = [os.path.join(sources_dir, s) for s in sources_]
 
     # Load extension
+
+    maybe_set_arch_list_env()
 
     exllamav2_ext = load \
     (
