b1973ee508d6dd0404ba8c271e0a2c41c6411ba0
turboderp <11859846+turboderp@users.noreply.github.com>
2024-05-18 15:44:15 +0200

    Dynamic gen: Update example

--- examples/dynamic_gen.py
+++ examples/dynamic_gen.py
@@ -1,20 +1,48 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 from exllamav2 import ExLlamaV2, ExLlamaV2Config, ExLlamaV2Cache, ExLlamaV2Tokenizer
 from exllamav2.generator import ExLlamaV2DynamicGenerator, ExLlamaV2DynamicJob, ExLlamaV2Sampler
-import time
 from blessed import Terminal
+import pprint
 
-model_dir = "/mnt/str/models/codellama-34b-instruct-exl2/4.0bpw/"
-total_context = 1024*6
+# Display modes for this demo:
+# 1: One line per job, updated continuously
+# 2: Print completions as jobs finish
+# 3: Step over output iteration by iteration
+# 4: Space heater mode (no output)
+display_mode = 1
+
+# Where to find our model
+model_dir = "/mnt/str/models/mistral-7b-instruct-exl2/4.0bpw"
+
+# Total number of tokens to allocate space for. This is not the max_seq_len supported by the model but
+# the total to distribute dynamically over however many jobs are active at once
+total_context = 16384
+
+# N-gram or draft model speculative decoding. Largely detrimental to performance at higher batch sizes.
 use_ngram = False
-use_draft_model = True
+use_draft_model = False
 draft_model_dir = "/mnt/str/models/tinyllama-1b-32k-exl2/4.0bpw"
-max_batch_size = 8
-max_new_tokens = 250
+
+# Max number of batches to run at once, assuming the sequences will fit within total_context.
+max_batch_size = 1
+
+# Max chunk size. Determines the size of prefill operations. Can be reduced to reduce pauses whenever a
+# new job is started, but at the expense of overall prompt ingestion speed.
 max_chunk_size = 2048
 
+# Max new tokens per completion. For this example applies to all jobs.
+max_new_tokens = 500
+
+# Use LMFE to constrain the output to JSON format. See schema and details below.
+json_mode = False
+
+# Demonstrate token healing
+healing = False
+
+# Some prompts to feed the generator
 prompt_format = "llama"
 system_prompt = "You are an AI assistant"
 prompts = [
@@ -27,6 +55,9 @@ prompts = [
     "Hi there!",
     "What's the difference smoke and vapor?",
     "What seems out of place in this sequence: " + ", ".join(str(n if n != 123 else 69) for n in range(200)),
+    "What seems out of place in this sequence: " + ", ".join(str(n if n != 42 else 111) for n in range(200)),
+    "What seems out of place in this sequence: " + ", ".join(str(n if n != 42 else 111) for n in range(200)),
+    "What seems out of place in this sequence: " + ", ".join(str(n if n != 42 else 111) for n in range(200)),
     "What seems out of place in this sequence: " + ", ".join(str(n if n != 42 else 111) for n in range(200)),
     "Please guess the next 20 numbers in this sequence: " + ", ".join(str(n) for n in range(700)),
     "Write a short essay about cell membranes.",
@@ -59,6 +90,10 @@ prompts = [
     "Hello!",
     "Hi!",
     "Boop!",
+    "Why are cats better than dogs?",
+    "Why are cats better than dogs?",
+    "Why are cats better than dogs?",
+    "Why are cats better than dogs?",
 ]
 
 term = Terminal()
@@ -81,6 +116,25 @@ def stop_conditions(tokenizer):
         return [tokenizer.eos_token_id]
     elif prompt_format == "llama3":
         return [tokenizer.single_id("<|eot_id|>")]
+
+
+# Only import lmfe if json_mode is set
+
+if json_mode:
+    import json
+    from lmformatenforcer.integrations.exllamav2 import ExLlamaV2TokenEnforcerFilter
+    from lmformatenforcer import JsonSchemaParser
+    from exllamav2.generator.filters import ExLlamaV2PrefixFilter
+    from pydantic import BaseModel
+    from typing import Literal
+
+    class JSONResponse(BaseModel):
+        response: str
+        confidence: Literal["low", "medium", "high"]
+        is_subjective: Literal["no", "yes", "possibly"]
+
+    schema_parser = JsonSchemaParser(JSONResponse.schema())
+
 
 def main():
 
@@ -141,14 +195,32 @@ def main():
 
     # Create jobs
 
+    if json_mode:
+        print("Creating jobs... (initializing JSON filters could take a moment.)")
+
     jobs = []
     for prompt in prompts:
-        input_ids = tokenizer.encode(format_prompt(system_prompt, prompt), encode_special_tokens = True)
+        if json_mode:
+            prompt += "\n\n Answer in JSON syntax."
+            filters = [
+                ExLlamaV2TokenEnforcerFilter(schema_parser, tokenizer),
+                ExLlamaV2PrefixFilter(model, tokenizer, "{")
+            ]
+        else:
+            filters = None
+        fprompt =  format_prompt(system_prompt, prompt)
+        if healing:
+            # To test/demonstrate healing, add a broken response prefix
+            fprompt += " The an"
+        input_ids = tokenizer.encode(fprompt, encode_special_tokens = True)
         job = ExLlamaV2DynamicJob(
             input_ids = input_ids,
             max_new_tokens = max_new_tokens,
             stop_conditions = stop_conditions(tokenizer),
-            gen_settings = ExLlamaV2Sampler.Settings()
+            gen_settings = ExLlamaV2Sampler.Settings(),
+            filters = filters,
+            filter_prefer_eos = True,
+            token_healing = healing
         )
         jobs.append(job)
 
@@ -156,7 +228,7 @@ def main():
 
     generator.enqueue(jobs)
 
-    # To see what's going on
+    # To see what's going on, mode 1
 
     class JobStatusDisplay:
 
@@ -191,7 +263,7 @@ def main():
             text += "   "
             text += term.green(f"{self.tokens: 5} t")
             text += term.black(" -> ")
-            text += (self.spaces + self.collected_output)[-80:]
+            text += (self.spaces + self.collected_output)[-80:].replace("\t", " ")
 
             if "accepted_draft_tokens" in r:
                 acc = r["accepted_draft_tokens"]
@@ -201,30 +273,60 @@ def main():
 
             print(term.move_xy(0, self.console_line) + text)
 
-    print(term.enter_fullscreen())
-    displays = { job: JobStatusDisplay(job, line) for line, job in enumerate(jobs) }
+    if display_mode == 1:
+        print(term.enter_fullscreen())
+        displays = { job: JobStatusDisplay(job, line) for line, job in enumerate(jobs) }
 
     # Streaming loop
 
     while generator.num_remaining_jobs():
         results = generator.iterate()
         for r in results:
-            job = r["job"]
-            displays[job].update(r)
+            if display_mode == 1:
+                job = r["job"]
+                displays[job].update(r)
+            if display_mode == 2:
+                if r["stage"] == "streaming" and r["eos"]:
+                    job = r["job"]
+                    print("\n")
+                    print(term.black("Input: "))
+                    print(term.yellow(tokenizer.decode(job.sequences[0].input_ids.torch(), decode_special_tokens = True)[0]))
+                    print()
+                    print(term.black("Output:"))
+                    if json_mode:
+                        try:
+                            print(json.dumps(json.loads(r["full_completion"]), indent = 4))
+                        except json.decoder.JSONDecodeError:
+                            print(term.red("Invalid JSON: ") + r["full_completion"])
+                    else:
+                        print(r["full_completion"])
+                    print()
+                    print(term.black("New tokens:  ") + term.green(f"{r['new_tokens']:7} t"))
+                    print(term.black("Enqueued:    ") + term.blue(f"{r['time_enqueued']:7.2f} s"))
+                    print(term.black("Prefill:     ") + term.blue(f"{r['time_prefill']:7.2f} s"))
+                    print(term.black("Generation:  ") + term.blue(f"{r['time_generate']:7.2f} s"))
+                    if "accepted_draft_tokens" in r:
+                        acc = r["accepted_draft_tokens"]
+                        rej = r["rejected_draft_tokens"]
+                        eff = acc / (acc + rej) * 100.0
+                        print(term.black("SD efficiency:") + term.bright_magenta(f"{eff:7.2f}%"))
+        if display_mode == 3 and len(r) > 0:
+            print()
+            pprint.pprint(results, indent = 4)
+            print()
+            print("Press any key to continue...")
+            with term.cbreak():
+                term.inkey()
 
-    print(term.move_xy(0, len(displays) + 1) + "Press any key to continue...")
-    with term.cbreak():
-        term.inkey()
+    if display_mode == 1:
+        print(term.move_xy(0, len(displays) + 1) + "Press any key to continue...")
+        with term.cbreak():
+            term.inkey()
 
 if __name__ == "__main__":
     try:
         main()
     finally:
         pass
-        print(term.exit_fullscreen())
-
-
-
-
-
-
+        if display_mode == 1:
+            print(term.exit_fullscreen())
