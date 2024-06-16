67c270c7242515c753ceb36059d8535fe799f982
turboderp <11859846+turboderp@users.noreply.github.com>
2024-06-16 16:13:42 +0200

    Improve AVX2 softmax approximation

--- exllamav2/exllamav2_ext/cpp/avx_mathfun.h
+++ exllamav2/exllamav2_ext/cpp/avx_mathfun.h
@@ -292,7 +292,7 @@ _PS256_CONST(cephes_exp_p4, 1.6666665459E-1);
 _PS256_CONST(cephes_exp_p5, 5.0000001201E-1);
 
 AVX2_TARGET
-v8sf exp256_ps(v8sf x) {
+v8sf exp256_ps_old(v8sf x) {
   v8sf tmp = _mm256_setzero_ps(), fx;
   v8si imm0;
   v8sf one = *(v8sf*)_ps256_1;
@@ -346,6 +346,94 @@ v8sf exp256_ps(v8sf x) {
   v8sf pow2n = _mm256_castsi256_ps(imm0);
   y = _mm256_mul_ps(y, pow2n);
   return y;
+}
+
+AVX2_TARGET
+
+__m256 exp256_ps(__m256 x) {
+/* Modified code from this source: https://github.com/reyoung/avx_mathfun
+
+   AVX implementation of exp
+   Based on "sse_mathfun.h", by Julien Pommier
+   http://gruntthepeon.free.fr/ssemath/
+   Copyright (C) 2012 Giovanni Garberoglio
+   Interdisciplinary Laboratory for Computational Science (LISC)
+   Fondazione Bruno Kessler and University of Trento
+   via Sommarive, 18
+   I-38123 Trento (Italy)
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  (this is the zlib license)
+
+*/
+/*
+  To increase the compatibility across different compilers the original code is
+  converted to plain AVX2 intrinsics code without ingenious macro's,
+  gcc style alignment attributes etc.
+  Moreover, the part "express exp(x) as exp(g + n*log(2))" has been significantly simplified.
+  This modified code is not thoroughly tested!
+*/
+
+
+__m256   exp_hi        = _mm256_set1_ps(88.3762626647949f);
+__m256   exp_lo        = _mm256_set1_ps(-88.3762626647949f);
+
+__m256   cephes_LOG2EF = _mm256_set1_ps(1.44269504088896341f);
+__m256   inv_LOG2EF    = _mm256_set1_ps(0.693147180559945f);
+
+__m256   cephes_exp_p0 = _mm256_set1_ps(1.9875691500E-4);
+__m256   cephes_exp_p1 = _mm256_set1_ps(1.3981999507E-3);
+__m256   cephes_exp_p2 = _mm256_set1_ps(8.3334519073E-3);
+__m256   cephes_exp_p3 = _mm256_set1_ps(4.1665795894E-2);
+__m256   cephes_exp_p4 = _mm256_set1_ps(1.6666665459E-1);
+__m256   cephes_exp_p5 = _mm256_set1_ps(5.0000001201E-1);
+__m256   fx;
+__m256i  imm0;
+__m256   one           = _mm256_set1_ps(1.0f);
+
+//        x     = _mm256_min_ps(x, exp_hi);  // Not needed since max value is 0
+        x     = _mm256_max_ps(x, exp_lo);
+
+  /* express exp(x) as exp(g + n*log(2)) */
+        fx     = _mm256_mul_ps(x, cephes_LOG2EF);
+        fx     = _mm256_round_ps(fx, _MM_FROUND_TO_NEAREST_INT |_MM_FROUND_NO_EXC);
+__m256  z      = _mm256_mul_ps(fx, inv_LOG2EF);
+        x      = _mm256_sub_ps(x, z);
+        z      = _mm256_mul_ps(x,x);
+
+__m256  y      = cephes_exp_p0;
+        y      = _mm256_mul_ps(y, x);
+        y      = _mm256_add_ps(y, cephes_exp_p1);
+        y      = _mm256_mul_ps(y, x);
+        y      = _mm256_add_ps(y, cephes_exp_p2);
+        y      = _mm256_mul_ps(y, x);
+        y      = _mm256_add_ps(y, cephes_exp_p3);
+        y      = _mm256_mul_ps(y, x);
+        y      = _mm256_add_ps(y, cephes_exp_p4);
+        y      = _mm256_mul_ps(y, x);
+        y      = _mm256_add_ps(y, cephes_exp_p5);
+        y      = _mm256_mul_ps(y, z);
+        y      = _mm256_add_ps(y, x);
+        y      = _mm256_add_ps(y, one);
+
+  /* build 2^n */
+        imm0   = _mm256_cvttps_epi32(fx);
+        imm0   = _mm256_add_epi32(imm0, _mm256_set1_epi32(0x7f));
+        imm0   = _mm256_slli_epi32(imm0, 23);
+__m256  pow2n  = _mm256_castsi256_ps(imm0);
+        y      = _mm256_mul_ps(y, pow2n);
+        return y;
 }
 
 _PS256_CONST(minus_cephes_DP1, -0.78515625);
