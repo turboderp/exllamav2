4c6dc5812a1fe08bd9a25e16dc022831581a4bde
turboderp <11859846+turboderp@users.noreply.github.com>
2024-08-20 11:50:10 +0200

    Disable unused code to fix compilation issues

--- exllamav2/exllamav2_ext/cuda/tp.cu
+++ exllamav2/exllamav2_ext/cuda/tp.cu
@@ -10,34 +10,37 @@ void init_tp_data(ExtTPData* tp_data)
     tp_data->next_stage = 0;
 }
 
-__global__ void cross_device_barrier_cuda_kernel
-(
-    uint32_t* sync,
-    uint32_t* sync_next,
-    uint32_t num_devices,
-    uint32_t device
-)
-{
-    // Arrive
-    sync[device] = 1;
+// Not used atm, disabled for compatibility
 
-    // Clear flags for next barrier
-    if (device == 0)
-        for (int i = 0; i < MAX_SYNC_DEVICES; ++i)
-            sync_next[i] = 0;
-
-    // Wait for other devices to arrive
-    int delay = 5;
-    while (true)
-    {
-        int i = 0;
-        for (; i < num_devices; ++i) if (i != device && sync[i] == 0) break;
-        if (i == num_devices) break;
-        __nanosleep(delay);
-        delay = min(delay * 2, 500);
-        __threadfence_system();
-    }
-}
+//__global__ void cross_device_barrier_cuda_kernel
+//(
+//    uint32_t* sync,
+//    uint32_t* sync_next,
+//    uint32_t num_devices,
+//    uint32_t device
+//)
+//{
+//
+//    // Arrive
+//    sync[device] = 1;
+//
+//    // Clear flags for next barrier
+//    if (device == 0)
+//        for (int i = 0; i < MAX_SYNC_DEVICES; ++i)
+//            sync_next[i] = 0;
+//
+//    // Wait for other devices to arrive
+//    int delay = 5;
+//    while (true)
+//    {
+//        int i = 0;
+//        for (; i < num_devices; ++i) if (i != device && sync[i] == 0) break;
+//        if (i == num_devices) break;
+//        __nanosleep(delay);
+//        delay = min(delay * 2, 500);
+//        __threadfence_system();
+//    }
+//}
 
 void cross_device_barrier_cuda
 (
@@ -48,11 +51,11 @@ void cross_device_barrier_cuda
     uint32_t device
 )
 {
-    cross_device_barrier_cuda_kernel<<<1, 1, 0, stream>>>
-    (
-        sync,
-        sync_next,
-        num_devices,
-        device
-    );
+//    cross_device_barrier_cuda_kernel<<<1, 1, 0, stream>>>
+//    (
+//        sync,
+//        sync_next,
+//        num_devices,
+//        device
+//    );
 }
