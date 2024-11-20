9cacd66229bcd1f0ea90e8a31657e7b1b4cc04c5
turboderp <11859846+turboderp@users.noreply.github.com>
2024-11-20 05:49:03 +0100

    Fix MRoPE model inference when no MM embeddings present

--- exllamav2/model.py
+++ exllamav2/model.py
@@ -954,7 +954,7 @@ class ExLlamaV2:
             seq_len <= self.config.max_output_len, \
             "seq_len exceeds max_output_len"
 
-        if self.config.arch.lm.mrope and "indexed_embeddings" in kwargs:
+        if self.config.arch.lm.mrope and kwargs.get("indexed_embeddings"):
             assert attn_params is not None and (
                 attn_params.rope_offsets is not None or
                 attn_params.get_alt_rope_embed("cpu") is not None
