9d77fecbfe273599a63d4a85c0be976dc2eb397b
nintwentydo <platform@prismmedia.io>
2024-12-21 11:10:40 +1100

    Fix multimodal_projector_bias to default to True if not in model config.json

--- exllamav2/architecture.py
+++ exllamav2/architecture.py
@@ -312,7 +312,7 @@ class ExLlamaV2ArchParams:
             })
             self.mmp.mlp_gate = False
             self.mmp.mlp_act_func = "gelu"
-            self.mmp.mlp_bias = read_config["multimodal_projector_bias"]
+            self.mmp.mlp_bias = bool(read_config.get("multimodal_projector_bias", True))
 
         # Yi
 
